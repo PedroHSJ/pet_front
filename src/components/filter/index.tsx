681ca84688ed555b02ba631d7e1f496b13f48dc0
Pedro Henrique <pedrohenriquesj.pro@gmail.com>
2023-08-21 22:01:30 -0300

    wip: Filter

--- src/components/filter/index.tsx
+++ src/components/filter/index.tsx
@@ -4,27 +4,76 @@ import { Form } from '../forms/Form';
 import { Button } from '../buttons/Button';
 import { InputComponent } from '../forms/NewInput';
 
-interface IFilterProps {
-    onSubmit: (data: any) => void;
+interface IOptions {
+    name?: string;
+    email?: string;
+    phone?: string;
+    crmv?: string;
+    cnpj?: string;
 }
 
-export const Filter = ({ onSubmit }: IFilterProps) => {
+interface IFilterProps {
+    options?: IOptions;
+}
+
+export const Filter = ({ options }: IFilterProps) => {
     const {
         control,
         handleSubmit,
         formState: { errors },
     } = useForm({});
 
+    const onSubmit = async (data: any) => {
+        console.log(data);
+    };
+
     return (
         <div className="flex shadow rounded my-4">
             <div className=" flex-1 mx-auto  px-4 py-6 sm:px-6 lg:px-8 ">
                 <Form onSubmit={handleSubmit(onSubmit)}>
-                    <InputComponent
-                        control={control}
-                        name="name"
-                        label="Nome"
-                        type="text"
-                    />
+                    <div className="flex gap-2  bg-red-50">
+                        {options?.name && (
+                            <InputComponent
+                                control={control}
+                                name="name"
+                                label={options.name}
+                                type="text"
+                            />
+                        )}
+                        {options?.email && (
+                            <InputComponent
+                                control={control}
+                                name="email"
+                                label={options.email}
+                                type="text"
+                            />
+                        )}
+                        {options?.phone && (
+                            <InputComponent
+                                control={control}
+                                name="phone"
+                                label={options.phone}
+                                type="text"
+                            />
+                        )}
+                        {options?.crmv && (
+                            <InputComponent
+                                control={control}
+                                name="crmv"
+                                label={options.crmv}
+                                type="text"
+                            />
+                        )}
+                        {options?.cnpj && (
+                            <InputComponent
+                                control={control}
+                                name="cnpj"
+                                label={options.cnpj}
+                                type="text"
+                            />
+                        )}
+                    </div>
+
                     <Button style="primary" type="submit">
                         Buscar
                     </Button>
