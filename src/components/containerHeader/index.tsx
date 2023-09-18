0eeedfef667978ab7f1cf906c3d6a0caee4ed228
Pedro Henrique <pedrohenriquesj.pro@gmail.com>
2023-09-18 11:25:27 -0300

    FIX: CONTAINER HEADER BUTTON

--- src/components/containerHeader/index.tsx
+++ src/components/containerHeader/index.tsx
@@ -3,11 +3,7 @@ interface IContainerHeaderProps {
     children?: React.ReactNode;
 }
 
-export const ContainerHeader = ({
-    title,
-    children,
-    button,
-}: IContainerHeaderProps) => {
+export const ContainerHeader = ({ title, children }: IContainerHeaderProps) => {
     return (
         <div className="flex flex-row rounded my-4 px-4 py-6 justify-between items-center">
             <h1 className="text-3xl font-bold tracking-tight text-gray-900">
