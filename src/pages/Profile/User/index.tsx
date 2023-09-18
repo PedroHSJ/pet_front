a751204b7d1a23ec313f2af0797801be54466115
Pedro Henrique <pedrohenriquesj.pro@gmail.com>
2023-09-18 11:30:11 -0300

    FEAT: PERFIL USER

--- src/pages/Profile/User/index.tsx
+++ src/pages/Profile/User/index.tsx
@@ -6,6 +6,7 @@ import {
     Typography,
 } from '@material-tailwind/react';
 import { IUser } from '../../../interfaces/IUser';
+import { UserIcon } from '@heroicons/react/24/solid';
 
 interface IProfileUserProps {
     user: IUser | null;
@@ -26,11 +27,15 @@ export const ProfileUser = ({ user }: IProfileUserProps) => {
                         shadow={false}
                         className="mx-0 flex items-center gap-4 pt-0 pb-8"
                     >
-                        <Avatar
+                        {/* <Avatar
                             size="lg"
                             variant="circular"
                             src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
                             alt="tania andrew"
+                        /> */}
+                        <UserIcon
+                            strokeWidth={6}
+                            className={`w-1/6 text-gray-600 border-2 rounded-full p-1`}
                         />
                         <div className="flex w-full flex-col gap-0.5">
                             <div className="flex items-center justify-between">
@@ -46,11 +51,8 @@ export const ProfileUser = ({ user }: IProfileUserProps) => {
                         </div>
                     </CardHeader>
                     <CardBody className="mb-6 p-0">
-                        <Typography>
-                            &quot;I found solution to all my design needs from
-                            Creative Tim. I use them as a freelancer in my hobby
-                            projects for fun! And its really affordable, very
-                            humble guys !!!&quot;
+                        <Typography color="blue-gray">
+                            {user?.role.name}
                         </Typography>
                     </CardBody>
                 </Card>
