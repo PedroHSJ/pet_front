785f8cd3e91ebde3478a25c32c6abba4070c6e3c
Pedro Henrique <pedrohenriquesj.pro@gmail.com>
2023-09-18 11:25:09 -0300

    FIX: CONTAINER HEADER

--- src/components/containerHeader/index.tsx
+++ src/components/containerHeader/index.tsx
@@ -9,7 +9,7 @@ export const ContainerHeader = ({
     button,
 }: IContainerHeaderProps) => {
     return (
-        <div className="flex flex-row shadow rounded my-4 px-4 py-6 justify-between">
+        <div className="flex flex-row rounded my-4 px-4 py-6 justify-between items-center">
             <h1 className="text-3xl font-bold tracking-tight text-gray-900">
                 {title}
             </h1>
